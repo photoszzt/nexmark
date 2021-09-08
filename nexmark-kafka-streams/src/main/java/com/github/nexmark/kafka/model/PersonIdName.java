829747ce8ac149c86080b51335a0079d810497be
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-09-08 01:25:51 -0500

    backup
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/model/PersonIdName.java
@@ -0,0 +1,11 @@
+package com.github.nexmark.kafka.model;
+
+public class PersonIdName {
+    public long id;
+    public String name;
+
+    public PersonIdName(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query8.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query8.java
@@ -1,14 +1,33 @@
 package com.github.nexmark.kafka.queries;
 
+import com.github.nexmark.kafka.model.Event;
+import com.github.nexmark.kafka.model.Person;
+import com.github.nexmark.kafka.model.PersonIdName;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
 
+import java.time.Duration;
 import java.util.Properties;
 
 public class Query8 implements NexmarkQuery {
     @Override
     public StreamsBuilder getStreamBuilder() {
-        return null;
+        StreamsBuilder builder = new StreamsBuilder();
+        JSONPOJOSerde<Event> serde = new JSONPOJOSerde<Event>() {
+        };
+        KStream<String, Event> inputs = builder.stream("nexmark-input", Consumed.with(Serdes.String(), serde)
+                .withTimestampExtractor(new JSONTimestampExtractor()));
+        KStream<PersonIdName, Event> person = inputs
+                .filter((key, value) -> value.type == Event.Type.PERSON)
+                .selectKey((key, value) ->
+                        new PersonIdName(value.newPerson.id,
+                                value.newPerson.name));
+        KStream<Long, Event> auction = inputs.filter((key, value) -> value.type == Event.Type.AUCTION)
+                .selectKey((key, value) -> value.newAuction.seller);
+        return builder;
     }
 
     @Override
