863b963e65d52bd31bf0803c9fb4fa803ae509aa
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-09-23 14:06:21 -0500

    backup fix
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query7.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query7.java
@@ -4,21 +4,16 @@ import com.github.nexmark.kafka.model.BidAndMax;
 import com.github.nexmark.kafka.model.Event;
 import com.github.nexmark.kafka.model.PriceTime;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.processor.ProcessorContext;
-import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.StoreBuilder;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Properties;
-import java.util.Set;
 
 public class Query7 implements NexmarkQuery {
     @Override
@@ -26,31 +21,25 @@ public class Query7 implements NexmarkQuery {
         StreamsBuilder builder = new StreamsBuilder();
         JSONPOJOSerde<Event> serde = new JSONPOJOSerde<Event>() {
         };
-        KStream<String, Event> inputs = builder.stream("nexmark-input", Consumed.with(Serdes.String(), serde)
-                .withTimestampExtractor(new JSONTimestampExtractor()));
+        KStream<String, Event> inputs = builder.stream("nexmark-input",
+                Consumed.with(Serdes.String(), serde).withTimestampExtractor(new JSONTimestampExtractor()));
         KStream<Long, Event> bid = inputs.filter((key, value) -> value.type == Event.Type.BID)
                 .selectKey((key, value) -> value.bid.price);
-        KTable<Windowed<Long>, PriceTime> maxBid = bid.groupByKey()
-                .windowedBy(TimeWindows.of(Duration.ofSeconds(10)))
-                .aggregate(
-                        () -> new PriceTime(0, Instant.MIN),
-                        (key, value, aggregate) -> {
-                            if (value.bid.price > aggregate.price) {
-                                return new PriceTime(value.bid.price,
-                                        value.bid.dateTime);
-                            } else {
-                                return aggregate;
-                            }
-                        }, Materialized.as("max-bid"));
+        bid.groupByKey().windowedBy(TimeWindows.of(Duration.ofSeconds(10)))
+                .aggregate(() -> new PriceTime(0, Instant.MIN), (key, value, aggregate) -> {
+                    if (value.bid.price > aggregate.price) {
+                        return new PriceTime(value.bid.price, value.bid.dateTime);
+                    } else {
+                        return aggregate;
+                    }
+                }, Materialized.as("max-bid"));
         bid.transformValues(new ValueTransformerWithKeySupplier<Long, Event, BidAndMax>() {
             public ValueTransformerWithKey get() {
                 return new ValueTransformerWithKey() {
-                    private ProcessorContext context;
                     private KeyValueStore stateStore;
 
                     @Override
                     public void init(ProcessorContext context) {
-                        this.context = context;
                         this.stateStore = (KeyValueStore) context.getStateStore("max-bid");
                     }
 
@@ -59,9 +48,8 @@ public class Query7 implements NexmarkQuery {
                         Event event = (Event) value;
                         PriceTime pt = (PriceTime) this.stateStore.get(readOnlyKey);
                         if (pt != null) {
-                            return new BidAndMax(event.bid.auction, event.bid.price,
-                                    event.bid.bidder, event.bid.dateTime, event.bid.extra,
-                                    pt.dateTime);
+                            return new BidAndMax(event.bid.auction, event.bid.price, event.bid.bidder,
+                                    event.bid.dateTime, event.bid.extra, pt.dateTime);
                         } else {
                             return null;
                         }
@@ -73,11 +61,10 @@ public class Query7 implements NexmarkQuery {
                     }
                 };
             }
-        }, "max-bid")
-                .filter((key, value) -> {
-                    Instant lb = value.maxDateTime.minus(10, ChronoUnit.SECONDS);
-                    return value.dateTime.compareTo(lb) >= 0 && value.dateTime.compareTo(value.maxDateTime) <= 0;
-                }).to("nexmark-q7", Produced.with(Serdes.Long(), new JSONPOJOSerde<BidAndMax>() {
+        }, "max-bid").filter((key, value) -> {
+            Instant lb = value.maxDateTime.minus(10, ChronoUnit.SECONDS);
+            return value.dateTime.compareTo(lb) >= 0 && value.dateTime.compareTo(value.maxDateTime) <= 0;
+        }).to("nexmark-q7", Produced.with(Serdes.Long(), new JSONPOJOSerde<BidAndMax>() {
         }));
         return builder;
     }
--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/RunQuery.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/RunQuery.java
@@ -1,59 +1,58 @@
 package com.github.nexmark.kafka.queries;
 
+import java.util.Properties;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 
-import java.util.Properties;
-
 public class RunQuery {
-    private static NexmarkQuery getNexmarkQuery(int queryNumber) {
-        switch (queryNumber) {
-            case 1:
-                return new Query1();
-            case 2:
-                return new Query2();
-            case 3:
-                return new Query3();
-            case 4:
-                return new Query4();
-            case 5:
-                return new Query5();
-            case 6:
-                return new Query6();
-            case 7:
-                return new Query7();
-            case 8:
-                return new Query8();
-            case 9:
-                return new WindowedAvg();
-            default:
-                System.err.println("Wrong query number: " + queryNumber);
-                return null;
-        }
+  private static NexmarkQuery getNexmarkQuery(int queryNumber) {
+    switch (queryNumber) {
+    case 1:
+      return new Query1();
+    case 2:
+      return new Query2();
+    case 3:
+      return new Query3();
+    case 4:
+      return new Query4();
+    case 5:
+      return new Query5();
+    case 6:
+      return new Query6();
+    case 7:
+      return new Query7();
+    case 8:
+      return new Query8();
+    case 9:
+      return new WindowedAvg();
+    default:
+      System.err.println("Wrong query number: " + queryNumber);
+      return null;
+    }
+  }
+
+  public static void main(final String[] args) {
+    if (args.length != 1) {
+      System.err.println("Need to specify query number");
+      System.exit(1);
+    }
+    System.out.println(args.length);
+    for (int i = 0; i < args.length; i++) {
+      System.out.println(args[i]);
     }
 
-    public static void main(final String[] args) {
-        if (args.length != 1) {
-            System.err.println("Need to specify query number");
-            System.exit(1);
-        }
-        System.out.println(args.length);
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-        
-        int queryNumber = Integer.parseInt(args[0]);
-        NexmarkQuery query = getNexmarkQuery(queryNumber);
-        if (query == null) {
-            System.exit(1);
-        }
-        StreamsBuilder builder = query.getStreamBuilder();
-        Properties props = query.getProperties();
-        Topology tp = builder.build();
-        System.out.println(tp.describe());
-        final KafkaStreams streams = new KafkaStreams(tp, props);
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-        streams.start();
+    int queryNumber = Integer.parseInt(args[0]);
+    NexmarkQuery query = getNexmarkQuery(queryNumber);
+    if (query == null) {
+      System.exit(1);
     }
+    StreamsBuilder builder = query.getStreamBuilder();
+    Properties props = query.getProperties();
+    Topology tp = builder.build();
+    System.out.println(tp.describe());
+    final KafkaStreams streams = new KafkaStreams(tp, props);
+    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    streams.start();
+  }
 }
