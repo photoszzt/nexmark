8cb9bf9894efcd8698119722e5b67e48d147d515
zhiting zhu <zhitingz@cs.utexas.edu>
2021-10-15 11:41:39 -0500

    use fault tolerant config
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/StreamsUtils.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/StreamsUtils.java
@@ -4,7 +4,9 @@ import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Collection;
@@ -21,6 +23,13 @@ public class StreamsUtils {
         props.put(StreamsConfig.DEFAULT_WINDOWED_KEY_SERDE_INNER_CLASS, JSONPOJOSerde.class);
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JSONPOJOSerde.class);
         props.put(StreamsConfig.DEFAULT_WINDOWED_VALUE_SERDE_INNER_CLASS, JSONPOJOSerde.class);
+
+        props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, "EXACTLY_ONCE_V2");
+        props.put(StreamsConfig.TOPOLOGY_OPTIMIZATION_CONFIG, "all");
+
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 3);
+        props.put(StreamsConfig.topicPrefix(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG), 2);
+        props.put(StreamsConfig.producerPrefix(ProducerConfig.ACKS_CONFIG), "all");
         return props;
     }
 
