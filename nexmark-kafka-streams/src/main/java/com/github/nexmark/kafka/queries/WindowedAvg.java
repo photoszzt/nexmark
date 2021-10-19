e1f6d030ba3fdb79a2ce6af6a577578dbef59324
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-10-18 23:36:56 -0500

    fix repartition
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/WindowedAvg.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/WindowedAvg.java
@@ -27,11 +27,14 @@ public class WindowedAvg implements NexmarkQuery {
     @Override
     public StreamsBuilder getStreamBuilder(String bootstrapServer) {
         int numPartition = 5;
-        NewTopic out = new NewTopic("windowedavg-out", numPartition, (short) 3);
+        short replicationFactor = 3;
+        NewTopic out = new NewTopic("windowedavg-out", numPartition, replicationFactor);
+        NewTopic groupByRepar = new NewTopic("windowedAvg-groupby-repar-repartition", numPartition, replicationFactor);
         // NewTopic storeTp = new NewTopic("windowedavg-agg-store", numPartition, (short) 3);
 
         ArrayList<NewTopic> newTps = new ArrayList<>(2);
         newTps.add(out);
+        newTps.add(groupByRepar);
         StreamsUtils.createTopic(bootstrapServer, newTps);
 
         StreamsBuilder builder = new StreamsBuilder();
