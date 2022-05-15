595b36a9f9821f4238c1f2bf56c665d8e0715c15
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-05-15 00:39:08 -0500

    backup q4 implementation
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query4.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query4.java
@@ -3,9 +3,10 @@ package com.github.nexmark.kafka.queries;
 import com.github.nexmark.kafka.model.AucIdCategory;
 import com.github.nexmark.kafka.model.AuctionBid;
 import com.github.nexmark.kafka.model.Event;
+import com.github.nexmark.kafka.model.SumAndCount;
+
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
@@ -21,14 +22,42 @@ public class Query4 implements NexmarkQuery {
     public StreamsBuilder getStreamBuilder(String bootstrapServer, String serde, String configFile) {
         StreamsBuilder builder = new StreamsBuilder();
         Serde<Event> eSerde;
+        Serde<AucIdCategory> aicSerde;
+        Serde<AuctionBid> abSerde;
+        Serde<SumAndCount> scSerde;
         if (serde.equals("json")) {
             JSONPOJOSerde<Event> eSerdeJSON = new JSONPOJOSerde<Event>();
             eSerdeJSON.setClass(Event.class);
             eSerde = eSerdeJSON;
+
+            JSONPOJOSerde<AucIdCategory> aicSerdeJSON = new JSONPOJOSerde<>();
+            aicSerdeJSON.setClass(AucIdCategory.class);
+            aicSerde = aicSerdeJSON;
+
+            JSONPOJOSerde<AuctionBid> abSerdeJSON = new JSONPOJOSerde<>();
+            abSerdeJSON.setClass(AuctionBid.class);
+            abSerde = abSerdeJSON;
+
+            JSONPOJOSerde<SumAndCount> scSerdeJSON = new JSONPOJOSerde<>();
+            scSerdeJSON.setClass(SumAndCount.class);
+            scSerde = scSerdeJSON;
+
         } else if (serde.equals("msgp")) {
             MsgpPOJOSerde<Event> eSerdeMsgp = new MsgpPOJOSerde<>();
             eSerdeMsgp.setClass(Event.class);
             eSerde = eSerdeMsgp;
+
+            MsgpPOJOSerde<AucIdCategory> aicSerdeMsgp = new MsgpPOJOSerde<>();
+            aicSerdeMsgp.setClass(AucIdCategory.class);
+            aicSerde = aicSerdeMsgp;
+
+            MsgpPOJOSerde<AuctionBid> abSerdeMsgp = new MsgpPOJOSerde<>();
+            abSerdeMsgp.setClass(AuctionBid.class);
+            abSerde = abSerdeMsgp;
+
+            MsgpPOJOSerde<SumAndCount> scSerdeMsgp = new MsgpPOJOSerde<>();
+            scSerdeMsgp.setClass(SumAndCount.class);
+            scSerde = scSerdeMsgp;
         } else {
             throw new RuntimeException("serde expects to be either json or msgp; Got " + serde);
         }
@@ -59,14 +88,42 @@ public class Query4 implements NexmarkQuery {
                                 .withKeySerde(Serdes.Long())
                                 .withValueSerde(eSerde));
 
-        auction.join(bid, (leftValue, rightValue) -> new AuctionBid(rightValue.bid.dateTime,
+        KeyValueBytesStoreSupplier maxBidsKV = Stores.inMemoryKeyValueStore("maxBidsKVStore");
+        KTable<AucIdCategory, Long> maxBids = auction
+                .join(bid, (leftValue, rightValue) -> new AuctionBid(rightValue.bid.dateTime,
                         leftValue.newAuction.dateTimeMs, leftValue.newAuction.expiresMs,
                         rightValue.bid.price, leftValue.newAuction.category))
                 .filter((key, value) -> value.bidDateTimeMs >= value.aucDateTimeMs
-                        && value.bidDateTimeMs <= value.aucExpiresMs
-                )
-                .groupBy((key, value) -> KeyValue.pair(new AucIdCategory(key, value.aucCategory), value));
-        // TODO: aggregate is not done
+                        && value.bidDateTimeMs <= value.aucExpiresMs)
+                .toStream()
+                .selectKey((key, value) -> new AucIdCategory(key, value.aucCategory))
+                .groupByKey(Grouped.with(aicSerde, abSerde))
+                .aggregate(() -> 0L, (key, value, aggregate) -> {
+                    if (value.bidPrice > aggregate) {
+                        return value.bidPrice;
+                    } else {
+                        return aggregate;
+                    }
+                }, Named.as("maxBidPrice"), Materialized.<AucIdCategory, Long>as(maxBidsKV)
+                        .withCachingEnabled()
+                        .withLoggingEnabled(new HashMap<>())
+                        .withKeySerde(aicSerde)
+                        .withValueSerde(Serdes.Long()));
+
+        KeyValueBytesStoreSupplier sumCountKV = Stores.inMemoryKeyValueStore("sumCountKVStore");
+        maxBids.toStream()
+                .selectKey((key, value) -> key.category)
+                .groupByKey(Grouped.with(Serdes.Long(), Serdes.Long()))
+                .aggregate(()->new SumAndCount(0, 0), 
+                (key, value, aggregate) -> new SumAndCount(aggregate.sum + value, aggregate.count+1),
+                Named.as("sumCount"), Materialized.<Long, SumAndCount>as(sumCountKV)
+                .withCachingEnabled()
+                        .withLoggingEnabled(new HashMap<>())
+                        .withKeySerde(Serdes.Long())
+                        .withValueSerde(scSerde))
+                .mapValues((key, value) -> (double)value.sum / (double) value.count)
+                .toStream()
+                .to("q4-out", Produced.with(Serdes.Long(), Serdes.Double()));
         return builder;
     }
 
