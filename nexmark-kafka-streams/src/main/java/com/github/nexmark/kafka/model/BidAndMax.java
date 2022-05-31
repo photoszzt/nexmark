573041738c6851ffb8433308086fef545671a2ce
Zhiting Zhu <zhitingz@cs.utexas.edu>
2022-05-31 16:30:50 -0500

    redo q7
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/model/BidAndMax.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/model/BidAndMax.java
@@ -8,21 +8,21 @@ public class BidAndMax {
     public long auction;
     public long bidder;
     public long dateTimeMs;
-    public String extra;
-    public long maxDateTime;
+    public long wStartMs;
+    public long wEndMs;
 
     @JsonCreator
     public BidAndMax(@JsonProperty("auction") long auction,
                      @JsonProperty("price") long price,
                      @JsonProperty("bidder") long bidder,
                      @JsonProperty("dateTime") long dateTime,
-                     @JsonProperty("extra") String extra,
-                     @JsonProperty("maxDateTime") long maxDateTime) {
+                     @JsonProperty("wStartMs") long wStartMs,
+                     @JsonProperty("wEndMs") long wEndMs) {
         this.auction = auction;
         this.price = price;
         this.bidder = bidder;
         this.dateTimeMs = dateTime;
-        this.extra = extra;
-        this.maxDateTime = maxDateTime;
+        this.wStartMs = wStartMs;
+        this.wEndMs = wEndMs;
     }
 }
--- nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query7.java
+++ nexmark-kafka-streams/src/main/java/com/github/nexmark/kafka/queries/Query7.java
@@ -2,7 +2,7 @@ package com.github.nexmark.kafka.queries;
 
 import com.github.nexmark.kafka.model.BidAndMax;
 import com.github.nexmark.kafka.model.Event;
-import com.github.nexmark.kafka.model.PriceTime;
+
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -10,14 +10,14 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.kstream.internals.TimeWindow;
 import org.apache.kafka.streams.state.*;
 
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.time.Duration;
-import java.time.Instant;
 import java.util.*;
+import static com.github.nexmark.kafka.queries.Constants.REPLICATION_FACTOR;
 
 public class Query7 implements NexmarkQuery {
     @Override
@@ -28,45 +28,58 @@ public class Query7 implements NexmarkQuery {
 
         String outTp = prop.getProperty("out.name");
         int numPar = Integer.parseInt(prop.getProperty("out.numPar"));
-        NewTopic out = new NewTopic(outTp, numPar, (short) 3);
+        NewTopic out = new NewTopic(outTp, numPar, REPLICATION_FACTOR);
 
-        String bidsTp = prop.getProperty("bids.name");
-        String bidsTpRepar = prop.getProperty("bids.reparName");
-        int bidsTpPar = Integer.parseInt(prop.getProperty("bids.numPar"));
-        NewTopic bidsRepar = new NewTopic(bidsTp, bidsTpPar, (short) 3);
+        String bidsByWinTp = prop.getProperty("bidsByWin.name");
+        String bidsByWinTpRepar = prop.getProperty("bidsByWin.reparName");
+        int bidsByWinTpPar = Integer.parseInt(prop.getProperty("bidsByWin.numPar"));
+        NewTopic bidsByWinRepar = new NewTopic(bidsByWinTp, bidsByWinTpPar, REPLICATION_FACTOR);
+
+        String bidsByPriceTp = prop.getProperty("bidsByPrice.name");
+        String bidsByPriceTpRepar = prop.getProperty("bidsByPrice.reparName");
+        int bidsByPriceTpPar = Integer.parseInt(prop.getProperty("bidsByPrice.numPar"));
+        NewTopic bidsByPriceRepar = new NewTopic(bidsByPriceTp, bidsByPriceTpPar, REPLICATION_FACTOR);
+
+        String maxBidsByPriceTp = prop.getProperty("maxBidsByPrice.name");
+        String maxBidsByPriceTpRepar = prop.getProperty("maxBidsByPrice.reparName");
+        int maxBidsByPriceTpPar = Integer.parseInt(prop.getProperty("maxBidsByPrice.numPar"));
+        NewTopic maxBidsByPriceRepar = new NewTopic(maxBidsByPriceTp, maxBidsByPriceTpPar, REPLICATION_FACTOR);
 
         List<NewTopic> nps = new ArrayList<>(2);
         nps.add(out);
-        nps.add(bidsRepar);
+        nps.add(bidsByWinRepar);
+        nps.add(bidsByPriceRepar);
+        nps.add(maxBidsByPriceRepar);
         StreamsUtils.createTopic(bootstrapServer, nps);
 
         Serde<Event> eSerde;
-        Serde<PriceTime> ptSerde;
         Serde<BidAndMax> bmSerde;
+        Serde<TimeWindow> twSerde;
         if (serde.equals("json")) {
             JSONPOJOSerde<Event> eSerdeJSON = new JSONPOJOSerde<Event>();
             eSerdeJSON.setClass(Event.class);
             eSerde = eSerdeJSON;
 
-            JSONPOJOSerde<PriceTime> ptSerdeJSON = new JSONPOJOSerde<>();
-            ptSerdeJSON.setClass(PriceTime.class);
-            ptSerde = ptSerdeJSON;
-
             JSONPOJOSerde<BidAndMax> bmSerdeJSON = new JSONPOJOSerde<>();
             bmSerdeJSON.setClass(BidAndMax.class);
             bmSerde = bmSerdeJSON;
+
+            JSONPOJOSerde<TimeWindow> twSerdeJSON = new JSONPOJOSerde<>();
+            twSerdeJSON.setClass(TimeWindow.class);
+            twSerde = twSerdeJSON;
+
         } else if (serde.equals("msgp")) {
             MsgpPOJOSerde<Event> eSerdeMsgp = new MsgpPOJOSerde<>();
             eSerdeMsgp.setClass(Event.class);
             eSerde = eSerdeMsgp;
 
-            MsgpPOJOSerde<PriceTime> ptSerdeMsgp = new MsgpPOJOSerde<>();
-            ptSerdeMsgp.setClass(PriceTime.class);
-            ptSerde = ptSerdeMsgp;
-
             MsgpPOJOSerde<BidAndMax> bmSerdeMsgp = new MsgpPOJOSerde<>();
             bmSerdeMsgp.setClass(BidAndMax.class);
             bmSerde = bmSerdeMsgp;
+
+            MsgpPOJOSerde<TimeWindow> twSerdeMsgp = new MsgpPOJOSerde<>();
+            twSerdeMsgp.setClass(TimeWindow.class);
+            twSerde = twSerdeMsgp;
         } else {
             throw new RuntimeException("serde expects to be either json or msgp; Got " + serde);
         }
@@ -74,77 +87,87 @@ public class Query7 implements NexmarkQuery {
         KStream<String, Event> inputs = builder.stream("nexmark_src",
                 Consumed.with(Serdes.String(), eSerde).withTimestampExtractor(new EventTimestampExtractor()));
 
-        KStream<Long, Event> bid = inputs.filter((key, value) -> value != null && value.etype == Event.EType.BID)
+        Duration windowSize = Duration.ofSeconds(10);
+        Duration grace = Duration.ofSeconds(5);
+
+        TimeWindows tw = TimeWindows.ofSizeAndGrace(windowSize, grace);
+
+        KStream<String, Event> bids = inputs.filter((key, value) -> value != null && value.etype == Event.EType.BID);
+        KStream<TimeWindow, Event> bidsByWin = bids
+                .selectKey(new KeyValueMapper<String, Event, TimeWindow>() {
+                    @Override
+                    public TimeWindow apply(String key, Event value) {
+                        long sizeMs = tw.sizeMs;
+                        long advanceMs = tw.advanceMs;
+                        long windowStart = (Math.max(0, value.bid.dateTime - sizeMs + advanceMs) / advanceMs)
+                                * advanceMs;
+                        long wEnd = windowStart + sizeMs;
+                        return new TimeWindow(windowStart, wEnd);
+                    }
+
+                })
+                .repartition(Repartitioned.<TimeWindow, Event>with(twSerde, eSerde)
+                        .withName(bidsByWinTpRepar)
+                        .withNumberOfPartitions(bidsByWinTpPar));
+        KStream<Long, Event> bidsByPrice = bids
                 .selectKey((key, value) -> value.bid.price)
                 .repartition(Repartitioned.with(Serdes.Long(), eSerde)
-                        .withName(bidsTpRepar)
-                        .withNumberOfPartitions(bidsTpPar));
+                        .withName(bidsByPriceTpRepar)
+                        .withNumberOfPartitions(bidsByPriceTpPar));
 
-        TimeWindows tw = TimeWindows.ofSizeAndGrace(Duration.ofSeconds(10), Duration.ofSeconds(5));
-        String wintabName = "max-bid-tab";
-        WindowBytesStoreSupplier maxBidWinStoreSupplier = Stores.inMemoryWindowStore(
-                wintabName, Duration.ofMillis(tw.size() + tw.gracePeriodMs()),
-                Duration.ofMillis(tw.size()), false);
-
-        bid.groupByKey(Grouped.with(Serdes.Long(), eSerde))
-                .windowedBy(tw)
-                .aggregate(() -> new PriceTime(0, Instant.MIN.toEpochMilli()), (key, value, aggregate) -> {
-                    if (value.bid.price > aggregate.price) {
-                        return new PriceTime(value.bid.price, value.bid.dateTime);
+        String maxBidPerWindowTabName = "maxBidByWinTab";
+        KeyValueBytesStoreSupplier maxBidPerWindowTabSupplier = Stores.inMemoryKeyValueStore(maxBidPerWindowTabName);
+        KStream<TimeWindow, Long> maxBidPerWin = bidsByWin
+                .groupByKey(Grouped.with(twSerde, eSerde))
+                .aggregate(() -> 0L, (key, value, aggregate) -> {
+                    if (value.bid.price > aggregate) {
+                        return value.bid.price;
                     } else {
                         return aggregate;
                     }
-                }, Materialized.<Long, PriceTime>as(maxBidWinStoreSupplier)
+                }, Materialized.<TimeWindow, Long>as(maxBidPerWindowTabSupplier)
                         .withCachingEnabled()
                         .withLoggingEnabled(new HashMap<>())
-                        .withKeySerde(Serdes.Long())
-                        .withValueSerde(ptSerde));
+                        .withKeySerde(twSerde)
+                        .withValueSerde(Serdes.Long()))
+                .toStream();
+        KStream<Long, TimeWindow> maxBidsByPrice = maxBidPerWin
+                .map(new KeyValueMapper<TimeWindow, Long, KeyValue<Long, TimeWindow>>() {
+                    @Override
+                    public KeyValue<Long, TimeWindow> apply(TimeWindow key, Long value) {
+                        return new KeyValue<Long, TimeWindow>(value, key);
+                    }
+                })
+                .repartition(Repartitioned.with(Serdes.Long(), twSerde)
+                        .withName(maxBidsByPriceTpRepar)
+                        .withNumberOfPartitions(maxBidsByPriceTpPar));
 
-        bid.transform(new TransformerSupplier<Long, Event, KeyValue<Long, BidAndMax>>() {
+        JoinWindows jw = JoinWindows.ofTimeDifferenceAndGrace(windowSize, grace);
+        Duration retension = Duration.ofMillis(jw.size() + jw.gracePeriodMs());
+        Duration winSize = Duration.ofMillis(jw.size());
+        WindowBytesStoreSupplier bByPStoreSupplier = Stores.inMemoryWindowStore(
+                "bidsByPrice-join-store", retension, winSize, true);
+        WindowBytesStoreSupplier maxBidsByPStoreSupplier = Stores.inMemoryWindowStore(
+                "maxBidsByPrice-join-store", retension, winSize, true);
+        bidsByPrice.join(maxBidsByPrice, new ValueJoiner<Event, TimeWindow, BidAndMax>() {
             @Override
-            public Transformer<Long, Event, KeyValue<Long, BidAndMax>> get() {
-                return new Transformer<Long, Event, KeyValue<Long, BidAndMax>>() {
-                    private TimestampedWindowStore<Long, PriceTime> stateStore;
-                    private ProcessorContext pctx;
-
-                    @Override
-                    public void init(ProcessorContext processorContext) {
-                        this.stateStore = (TimestampedWindowStore<Long, PriceTime>) processorContext
-                                .getStateStore(wintabName);
-                        this.pctx = processorContext;
-                    }
-
-                    @Override
-                    public KeyValue<Long, BidAndMax> transform(Long aLong, Event event) {
-                        WindowStoreIterator<ValueAndTimestamp<PriceTime>> ptIter = this.stateStore.fetch(aLong,
-                                event.bid.dateTime - 10*1000, event.bid.dateTime);
-                        while (ptIter.hasNext()) {
-                            KeyValue<Long, ValueAndTimestamp<PriceTime>> kv = ptIter.next();
-                            if (event.bid.price == kv.value.value().price) {
-                                pctx.forward(aLong, new BidAndMax(event.bid.auction, event.bid.price, event.bid.bidder,
-                                        event.bid.dateTime, event.bid.extra, kv.value.value().dateTime));
-                            }
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    public void close() {
-                    }
-                };
+            public BidAndMax apply(Event value1, TimeWindow value2) {
+                return new BidAndMax(value1.bid.auction, value1.bid.price,
+                        value1.bid.bidder, value1.bid.dateTime, value2.start(), value2.end());
             }
-        }, wintabName)
-                .filter((key, value) -> {
-                    long lb = value.maxDateTime - 10*1000;
-                    return value.dateTimeMs >= lb && value.dateTimeMs <= value.maxDateTime;
-                }).to(outTp, Produced.with(Serdes.Long(), bmSerde));
+        }, jw, StreamJoined.<Long, Event, TimeWindow>with(bByPStoreSupplier, maxBidsByPStoreSupplier)
+                .withKeySerde(Serdes.Long())
+                .withValueSerde(eSerde)
+                .withOtherValueSerde(twSerde)
+                .withLoggingEnabled(new HashMap<>())).to(outTp, Produced.with(Serdes.Long(), bmSerde));
+
         return builder;
     }
 
     @Override
     public Properties getProperties(String bootstrapServer, int duration, int flushms) {
         Properties props = StreamsUtils.getStreamsConfig(bootstrapServer, duration, flushms);
-        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "nexmark-q7");
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "q7");
         return props;
     }
 
